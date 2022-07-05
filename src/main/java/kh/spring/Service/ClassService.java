84833815baa036131e692df94f17a0a03bd0e987
Yunji <yjjung9494@hanmail.net>
2022-07-05 14:41:02 +0900

    ImgService & ClassService modified

--- src/main/java/kh/spring/Service/ClassService.java
+++ src/main/java/kh/spring/Service/ClassService.java
@@ -8,10 +8,10 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 
 import kh.spring.DAO.ClassDAO;
-import kh.spring.DAO.FilesDAO;
+import kh.spring.DAO.ImgDAO;
 import kh.spring.DAO.SeqDAO;
 import kh.spring.DTO.ClassDTO;
-import kh.spring.DTO.FilesDTO;
+import kh.spring.DTO.ImgDTO;
 
 @Service
 public class ClassService {
@@ -20,7 +20,7 @@ public class ClassService {
 	private ClassDAO cdao;
 	
 	@Autowired
-	private FilesDAO fdao;
+	private ImgDAO idao;
 	
 	@Autowired
 	private SeqDAO sdao;
@@ -37,15 +37,15 @@ public class ClassService {
 		cdto.setClass_seq(class_seq);
 		cdao.insert(cdto);
 		
-		//파일 업로드		
-		
+		//파일 업로드				
 		JsonArray arr = g.fromJson(arrImg, JsonArray.class);
 		for(int i=0;i<arr.size();i++) {
-			FilesDTO fdto = g.fromJson(arr.get(i), FilesDTO.class);
-			fdto.setParent_seq(class_seq);
+			ImgDTO idto = g.fromJson(arr.get(i), ImgDTO.class);
+			idto.setParent_seq(class_seq);
 			
-			String seq =sdao.getSeq(fdto.getImg_seq());
-			fdao.insert(fdto,seq);
+			String seq =sdao.getSeq(idto.getImg_seq());
+			idto.setImg_seq(seq);
+			idao.insert(idto);
 		}
 		
 		// detailview 보여줄 seq 리턴
--- src/main/java/kh/spring/Service/ImgService.java
+++ src/main/java/kh/spring/Service/ImgService.java
@@ -1,7 +1,13 @@
 package kh.spring.Service;
 
+import java.io.File;
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import kh.spring.DAO.ImgDAO;
 
@@ -9,4 +15,25 @@ import kh.spring.DAO.ImgDAO;
 public class ImgService {
 	@Autowired
 	private ImgDAO dao;
+	
+	@Autowired
+	private HttpSession session;
+	
+	public String addClassFile(MultipartFile file) throws Exception{
+		String realPath = session.getServletContext().getRealPath("upload"); // 파일 업로드 경로 설정
+
+		File realPathFile = new File(realPath); 
+		if(!realPathFile.exists()) {realPathFile.mkdir();} // 경로가 존재하지 않는다면 생성
+		
+		String ori_name = file.getOriginalFilename();
+		String sys_name = UUID.randomUUID()+"_"+ori_name; 		
+		file.transferTo(new File(realPath+"/"+sys_name)); 
+		
+		return "{\"ori_name\":\""+ori_name+"\",\"sys_name\":\""+sys_name+"\"}";
+	}
+	
+	public Boolean deleteClassFile(String sys_name) {
+		String realPath = session.getServletContext().getRealPath("upload");
+		return new File(realPath+"/"+sys_name).delete();
+	}
 }
