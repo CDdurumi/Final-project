d53fdb88b604a3bae2968cb46b4e339ec0d92b0d
Yunji <yjjung9494@hanmail.net>
2022-07-06 20:14:12 +0900

    classController modified

--- src/main/java/kh/spring/Controller/ClassController.java
+++ src/main/java/kh/spring/Controller/ClassController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.google.gson.Gson;
 
 import kh.spring.DTO.ClassDTO;
+import kh.spring.DTO.ImgDTO;
 import kh.spring.Service.ClassService;
 import kh.spring.Service.ImgService;
 
@@ -60,7 +62,7 @@ public class ClassController {
 		return iServ.deleteClassFile(sys_name).toString();
 	}
 	
-	// 글 업로드
+	// 클래스 글 업로드 (ajax)
 	@ResponseBody
 	@RequestMapping(value="upload",produces="text/html;charset=utf8")
 	public String upload(ClassDTO cdto, String arrImg) throws Exception{
@@ -70,21 +72,97 @@ public class ClassController {
 		return cServ.insert(cdto,arrImg);		
 	}
 	
-	
+	// 클래스 글 읽기
 	@RequestMapping("detail")
-	public String detail(String class_seq) {
-		Map<String, String> map = new HashMap<>();
-		ClassDTO cdto = cServ.selectBySeq(class_seq);
+	public String detail(String class_seq,Model model) throws Exception{
+		
+		//ClassDTO 와 List<ImgDTO>,찜 여부를 json화 해서 받아옴
+		Map<String, String> map = cServ.selectDetailBySeq(class_seq);
+		
+		//json을 classDTO로 변환하여 model에 담기
+		model.addAttribute("cdto", g.fromJson(map.get("cdto"), ClassDTO.class));
+		
+		//ImgDTO를 담은 jsonArray model에 담기
+		model.addAttribute("arrImg",map.get("arrImg"));
+		
+		//사용자의 해당 클래스 찜 여부를 model에 담기
+		model.addAttribute("likeOrNot",map.get("likeOrNot"));
+		
 		return "/class/classDetail";
 	}
+	 
+	// 클래스 찜기능 (ajax)
+	@RequestMapping("like")
+	public void like(String parent_seq) throws Exception{		
+		// String email = (String)session.getAttribute("loginID");		
+		String email = "yjjung9494@hanmail.net";
+		cServ.like(email,parent_seq);
+	}
 	
-	@RequestMapping("reg")
-	public String reg() {
+	// 클래스 찜 취소 기능 (ajax)
+	@RequestMapping("likeCancel")
+	public void likeCancel(String parent_seq) throws Exception{		
+		// String email = (String)session.getAttribute("loginID");
+		String email = "yjjung9494@hanmail.net";
+		cServ.likeCancel(email,parent_seq);
+	}
+	
+	// 클래스 구매 여부 (ajax)
+	@ResponseBody
+	@RequestMapping("regOrNot")
+	public Boolean regOrNot(String parent_seq) throws Exception{
+		
+		// String std_id = (String)session.getAttribute("loginID");
+		String std_id = "yjjung9494@hanmail.net";
+		Boolean regOrNot = false;
+		if(cServ.regOrNot(std_id,parent_seq)>0) {
+			regOrNot = true;
+		}
+		return regOrNot;		
+	}
+
+	// 클래스 구매 페이지로 이동
+	@RequestMapping("toReg")
+	public String reg(String class_seq,Model model) throws Exception{		
+		//ClassDTO 와 메인 이미지 ImgDTO를 json화 해서 받아옴
+		Map<String, String> map = cServ.selectRegBySeq(class_seq);
+		
+		//json을 classDTO로 변환하여 model에 담기
+		model.addAttribute("cdto", g.fromJson(map.get("cdto"), ClassDTO.class));
+		
+		//json을 ImgDTO로 변환하여 model에 담기
+		model.addAttribute("idto", g.fromJson(map.get("idto"), ImgDTO.class));
+		
 		return "/class/classReg";
 	}
 	
+	// 클래스 구매 처리(ajax)
+	@ResponseBody
+	@RequestMapping("reg")
+	public Boolean regOrNot(String parent_seq,String type) throws Exception{
+		
+		// String std_id = (String)session.getAttribute("loginID");
+		String std_id = "yjjung9494@hanmail.net";
+		Boolean regFin = false;
+		if(cServ.reg(std_id, type, parent_seq)>0) {
+			regFin=true;
+		}
+		return regFin;
+	}
+	
+	//결제 완료 페이지로 이동
 	@RequestMapping("regFin")
-	public String regFin() {
+	public String regFin(String class_seq,Model model) throws Exception{
+		
+		//ClassDTO 와 메인 이미지 ImgDTO를 json화 해서 받아옴
+		Map<String, String> map = cServ.selectRegBySeq(class_seq);
+		
+		//json을 classDTO로 변환하여 model에 담기
+		model.addAttribute("cdto", g.fromJson(map.get("cdto"), ClassDTO.class));
+		
+		//json을 ImgDTO로 변환하여 model에 담기
+		model.addAttribute("idto", g.fromJson(map.get("idto"), ImgDTO.class));
+				
 		return "/class/classRegF";
 	}
 	
